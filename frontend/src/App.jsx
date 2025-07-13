3beb6432c78a2ce412ea14e57f560592eeb87bd2
Adarsh Agrahari <adarshagrahari718@gmail.com>
2025-07-14 00:45:22 +0530

    Replace / route with Signin component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<h1>Hello from Home Page</h1>} />
+          <Route path="/" element={<Signin/>} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/dashboard" element={<Dashboard />} />
