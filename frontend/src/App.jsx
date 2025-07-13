a04886c3b1080f72a8a69fdbffb069025790a5d4
Adarsh Agrahari <adarshagrahari718@gmail.com>
2025-07-14 00:39:00 +0530

    something happen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,19 +11,16 @@ import { SendMoney } from "./pages/SendMoney";
 
 function App() {
   return (
-    <>
-       <BrowserRouter>
-        <Routes>
-          
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} />
-        </Routes>
-      </BrowserRouter>
-  
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<h1>Hello from Home Page</h1>} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
